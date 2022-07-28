82310e8204ff357907c01189321a8fac5c6f47a4
zym0tic <lukerozon@gmail.com>
2022-07-28 18:16:56 -0400

    started conditional for past/present/future

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -13,31 +13,42 @@
 // THEN the saved events persist
 
 // setting date
-var currentDay = ('#currentDay');
-var currentTime = moment().format('LT');
-var hour = document.querySelector('.hour');
-var btn1 = $('.saveBtn1');
-var btn2 = $('.saveBtn2');
-var btn3 = $('.saveBtn3');
-var btn4 = $('.saveBtn4');
-var btn5 = $('.saveBtn5');
-var btn6 = $('.saveBtn6');
-var btn7 = $('.saveBtn7');
-var btn8 = $('.saveBtn8');
-var btn9 = $('.saveBtn9');
-var saveBtn = [btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9];
-var timeBlock = $('.time-block');
-var container = $('.container');
+var currentDay = "#currentDay";
+var currentTime = moment().format("LT");
+var hour = document.querySelector(".hour");
+var today = moment().format("H");
+var timeBlock = $(".time-block");
+var container = $(".container");
+var textAreas = [
+  $("#9"),
+  $("#10"),
+  $("#11"),
+  $("#12"),
+  $("#13"),
+  $("#14"),
+  $("#15"),
+  $("#16"),
+  $("#17"),
+];
+var textAreaNum = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
+$(currentDay).text(moment().format("MMMM Do YYYY"));
 
-$(currentDay).text(moment().format('MMMM Do YYYY'));
+container.on("click", ".btn", function (event) {
+  var activity = $(event.target).siblings("textarea").val();
 
-
-
-container.on('click', '.btn', function(event) {
-var activity = $(event.target).siblings("textarea").val();
-console.log(activity);
-
-// localStorage.setItem('activity', activity);
+  localStorage.setItem("activity", activity);
 });
 
+console.log(today);
+console.log(textAreaNum);
+
+for (i = 0; i < textAreas.length; i++) {
+if (today === textAreaNum[i]) {
+    textAreas[i].addClass("present");
+} else if (today > textAreaNum[i]) {
+    textAreas[i].addClass("past");
+} else {
+    textAreas[i].addClass("future");
+};
+}
